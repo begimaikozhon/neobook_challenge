920492db8291717f2c81579b9de5d276e498f77d
begimaikozhon <kozhonova.begimai.kanybekovna@gmail.com>
2023-12-17 23:31:24 +0600

    add

--- lib/src/screen/product_list_screen.dart
+++ lib/src/screen/product_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neobook_challenge/src/themes/app_colors.dart';
 
 class ProductListScreen extends StatelessWidget {
   const ProductListScreen({Key? key}) : super(key: key);
@@ -7,15 +8,39 @@ class ProductListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
         title: const Text(
           'Продукты',
           style: TextStyle(fontWeight: FontWeight.w500),
-          
         ),
-        actions: [
-          
-        ],
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.search,
+                      color: AppColors.grey,
+                      size: 40,
+                    ),
+                  ),
+                  hintText: 'Быстрый поиск',
+                  hintStyle: TextStyle(color: AppColors.grey, fontSize: 16),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
